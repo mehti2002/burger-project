2fae3e004033fd0c640b2e04458720896e78821a
Mehdi Mohammdi <maxmehti2@gmail.com>
2025-08-13 12:57:18 +0330

    feat: install sass loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <h1>مینی پروژه vista</h1>
-  </div>
+  <main class="home-page"></main>
 </template>
 
 <script setup>
 </script>
-
-<style scoped>
-h1 {
-  color: #007acc;
+<style lang="scss" scoped>
+body {
+  margin: 0px !important;
 }
 </style>
+
